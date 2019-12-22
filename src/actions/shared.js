1d01ce3bd3a932134cdc4eb9c3390e2865ca0a4d
Soria <soriasayer@gmail.com>
2019-12-22 21:08:54 +0100

    It is a clone of tweeter with react-redux

---
+++ src/actions/shared.js
@@ -0,0 +1,20 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+import { authedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+const AUTHED_ID = 'tylermcginniser'
+
+export function handleInitialData() {
+    return (dispatch) => {
+        dispatch(showLoading())
+        return getInitialData()
+        .then(({users, tweets}) => {
+            dispatch(receiveUsers(users))
+            dispatch(receiveTweets(tweets))
+            dispatch(authedUser(AUTHED_ID))
+            dispatch(hideLoading())
+        })
+    }
+}
